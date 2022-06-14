a181c4bb2525042a6051f3f5bb047b2ecb725d9b
Tosho K <tosekleckaroski@gmail.com>
2022-06-14 22:27:16 +0200

    main task fix

---
+++ Test-1/Ex-2/main.py
@@ -0,0 +1,96 @@
+from searching_framework.utils import Problem
+from searching_framework.uninformed_search import *
+import bisect
+
+"""
+Дефинирање на класа за структурата на проблемот кој ќе го решаваме со пребарување.
+Класата Problem е апстрактна класа од која правиме наследување за дефинирање на основните 
+карактеристики на секој проблем што сакаме да го решиме
+"""
+
+
+class Search(Problem):
+    def __init__(self,initial, goal=None):
+        super().__init__(initial, goal)
+
+
+    def successor(self, state):
+        """За дадена состојба, врати речник од парови {акција : состојба}
+        достапни од оваа состојба. Ако има многу следбеници, употребете
+        итератор кој би ги генерирал следбениците еден по еден, наместо да
+        ги генерирате сите одеднаш.
+        :param state: дадена состојба
+        :return:  речник од парови {акција : состојба} достапни од оваа
+                  состојба
+        :rtype: dict
+        """
+        successors = dict()
+
+
+
+
+        if
+
+
+
+
+        return successors
+
+
+
+
+    def actions(self, state):
+        """За дадена состојба state, врати листа од сите акции што може да
+        се применат над таа состојба
+        :param state: дадена состојба
+        :return: листа на акции
+        :rtype: list
+        """
+        return self.successor(state).keys()
+
+    def result(self, state, action):
+        """За дадена состојба state и акција action, врати ја состојбата
+        што се добива со примена на акцијата над состојбата
+        :param state: дадена состојба
+        :param action: дадена акција
+        :return: резултантна состојба
+        """
+        return self.successor(state)[action]
+
+    def goal_test(self, state):
+        """Врати True ако state е целна состојба. Даденава имплементација
+        на методот директно ја споредува state со self.goal, како што е
+        специфицирана во конструкторот. Имплементирајте го овој метод ако
+        проверката со една целна состојба self.goal не е доволна.
+        :param state: дадена состојба
+        :return: дали дадената состојба е целна состојба
+        :rtype: bool
+        """
+        return len(state[-1]) == 0
+
+
+
+if __name__ == "__main__":
+
+    diskovi = []
+    x = 1
+    n = int(input())
+    l = int(input())
+
+    for i in range(0, n):
+        diskovi.append(int(x))
+        x += 1
+
+    for i in range(n, l):
+        diskovi.append('')
+
+    diskovi = tuple(diskovi)
+
+
+
+
+
+
+
+
+
